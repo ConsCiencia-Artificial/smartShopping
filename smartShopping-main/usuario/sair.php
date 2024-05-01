b6ede114898b38837a8dc473db566c16dcff3dd7
Gabrielleschi <168485690+Gabrielleschi@users.noreply.github.com>
2024-05-01 16:26:54 -0300

    Delete smartShopping-main directory

--- smartShopping-main/usuario/sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-ob_start();
-unset($_SESSION['codigo'], $_SESSION['nome_usuario']);
-$_SESSION['msg'] = "<p style='color: #green'> Deslogado com sucesso! </p>";
-header("Location:../index.php");
