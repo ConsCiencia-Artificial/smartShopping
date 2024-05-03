a1ef788e5797d2a41d1d0f04a03894fb6a767b3f
Thiago Augusto Manzoni <thiagomanzoni@hotmail.com>
2024-05-03 15:28:30 -0300

    arrumando o back end
    recoloquei o form no login e estou arrumando o dashboard

--- usuario/login.php
+++ usuario/login.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-ob_start();
-include_once '../conexao.php';
 ?>
 
 <!DOCTYPE html>
@@ -27,16 +25,17 @@ include_once '../conexao.php';
       <h2 style="padding-top: 12px;">ENTRAR</h2>
     </div>
     <div class='box-login'>
+    <form action="" method="POST">
       <div class='fieldset-body' id='login_form'>
         <button onclick="openLoginInfo();" class='b b-form i i-more' title='Mais Informações'></button>
         <p class='field'>
           <label for='user'>E-MAIL</label>
-          <input type='text' id='user' name='user' title='Email' />
+          <input type='text' id='user' name='email_usuario' title='Email' value="<?php if(isset($dados['email_usuario'])){ echo $dados['email_usuario'];}?>" />
           <span id='valida' class='i i-warning'></span>
         </p>
         <p class='field'>
           <label for='pass'>SENHA</label>
-          <input type='password' id='pass' name='pass' title='Senha' />
+          <input type='password' id='pass' name='senha_usuario' title='Senha' value="<?php if(isset($dados['senha_usuario'])){ echo $dados['senha_usuario'];}?>" />
           <span id='valida' class='i i-close'></span>
         </p>
 
@@ -44,8 +43,9 @@ include_once '../conexao.php';
           <input type='checkbox' value='TRUE' title='Mantenha-me conectado' /> Mantenha-me conectado
         </label>
 
-        <input type='submit' id='do_login' value='ENTRAR' title='Entrar' />
+        <input type='submit' id='do_login' name="SendLogin" value='ENTRAR' title='Entrar' />
       </div>
+      </form>
     </div>
   </div>
   <div class='box-info'>
@@ -61,39 +61,7 @@ include_once '../conexao.php';
   </div>
 </div>
 
-    <?php
-    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-    ?>
-    <!-- ANTIGO LOGIN
-
-        <div class="login container-fluid row position-absolute top-50 start-50 translate-middle">
-        <div class="col-sm"></div>
-        <div class="col-sm container-fluid text-center border border-dark rounded d-flex mx-auto">
-            <main class="form-signin w-100 m-auto mt-5">
-                <form action="dashboard.php" method="POST">
-                    <h1 class="display-4 text-dark mb-3 animated slideInDown">Login</h1>
-                    
-                    <div class="form-floating" style="padding-top: 2rem;">
-                        <input type="email" class="form-control senha" id="floatingInput" placeholder="name@example.com" name="email_usuario" value="<?php if (isset($dados['email_usuario'])) {
-                                                                                                                                                            echo $dados['email_usuario'];
-                                                                                                                                                        } ?>">
-                        <label for="floatingInput" style="padding-top: 3rem;">Email</label>
-                    </div>
-                    <div class="form-floating" style="padding-top: 2rem;">
-                        <input type="password" class="form-control senha" id="floatingPassword" placeholder="Password" name="senha_usuario" value="<?php if (isset($dados['senha_usuario'])) {
-                                                                                                                                                        echo $dados['senha_usuario'];
-                                                                                                                                                    } ?>">
-                        <label for="floatingPassword" style="padding-top: 3rem;">Senha</label>
-                    </div>
-                    <div style="padding-top: 2rem;">
-                        <input class="btn btn-primary w-100 py-2 mb-4" href="" type="submit" name="SendLogin" value="Logar">
-                        <a type="submit" href="criarconta.php" class="btn btn-primary w-100 py-2 mb-5">Criar conta</a>
-                    </div>
-                </form>
-            </main>
-        </div>
-        <div class="col-sm"></div>
-    </div> -->
+   
     <script type="text/javascript" src="login.js"></script>
 </body>
 
@@ -107,37 +75,27 @@ include_once '../conexao.php';
 
 
 //'".$dados['usuario']."'
-if (!empty($dados['SendLogin'])) {
+if (!empty($_POST)) 
+{	
+    
+    $email = $_POST['email_usuario'];
+    $senha = $_POST['senha_usuario'];
 
+   include_once('../conexao.php');
 
-    //var_dump($dados);
-    //                                             TEM PORRA ERRADA AQUI kkkkkkkk JA ARRUMEI 
-    $query_usuario = "SELECT codigo, nome_usuario, email_usuario, senha_usuario 
-    FROM tb_cadastro_usuario 
-    WHERE email_usuario = :email_usuario
-    LIMIT 1";
-
-
-    $result_usuario = $conn->prepare($query_usuario);
-    $result_usuario->bindParam(':email_usuario', $dados['email_usuario'], PDO::PARAM_STR);
-    $result_usuario->execute();
-
-    isset($_SESSION['codigo'], $_SESSION['nome_usuario']);
-
-    if (($result_usuario) and ($result_usuario->rowCount() != 0)) {
-        $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
-        var_dump($row_usuario);
-        if (password_verify($dados['senha_usuario'], $row_usuario['senha_usuario'])) {
-            $_SESSION['codigo'] = $row_usuario['codigo'];
-            $_SESSION['nome_usuario'] = $row_usuario['nome_usuario'];
-            header("Location: dashboard.php");
-        }
-    } else {
-        $_SESSION['msg'] = "<p style='color: #ff0000'> Error: Usuário ou senha invalida! </p>";
-    }
-}
-if (isset($_SESSION['msg'])) {
-    echo $_SESSION['msg'];
-    unset($_SESSION['msg']);
+   
+   $select = $conn->prepare("SELECT * FROM tb_cadastro_usuario where email_usuario='$email'and 
+   senha_usuario='$senha'");
+   $select->execute();
+ 
+   $row = $select->fetch();
+   
+      
+       session_start();
+       $_SESSION["email_usuario"]= $row['email_usuario'];
+       $_SESSION['nome_usuario']= $row['nome_usuario'];
+      //var_dump($_SESSION);
+      $conn = null;
+        header('Location: dashboard.php');
 }
 ?>
